30aa1eb68eb94a75e54172889721f759294b8bec
MoonDropX <minalinskyx@hotmail.com>
2018-11-23 22:18:19 +0800

    fix(uploadOrigin): add md5 check for file diff

--- app/middlewares/uploadOrigin.ts
+++ app/middlewares/uploadOrigin.ts
@@ -2,8 +2,16 @@ import { Context } from 'koa';
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as dbs from '../graphql/dataFiles';
+import * as crypto from 'crypto';
 import { DATA_DIR } from '../consts';
 import MutationResolver from '../graphql/resolvers/MutationResolver';
+
+function md5hex(str: string | Buffer) {
+  return crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex');
+}
 
 async function writeFile(filename: string, file: any) {
   return new Promise((resolve, reject) => {
@@ -29,32 +37,37 @@ export default async (ctx: Context, next: any) => {
       files.CardList &&
       files.QuestList
     ) {
-      // copy old filelist for diff
-      const filelistStat = await fs.stat(path.join(DATA_DIR, 'FileList.json'));
-      if (
-        Math.abs(filelistStat.ctime.getTime() - new Date().getTime()) /
-          1000 /
-          3600 /
-          24 >=
-        4
-      ) {
-        await fs.copy(
-          path.join(DATA_DIR, 'FileList.json'),
-          path.join(DATA_DIR, 'FileListOld.json'),
-        );
-      }
+      await fs.copy(
+        path.join(DATA_DIR, 'FileList.json'),
+        path.join(DATA_DIR, 'FileListTmp.json'),
+      );
+      // }
       await Promise.all([
         writeFile('FileList.json', files.FileListR),
         writeFile('FileListA.json', files.FileListA),
         writeFile('CardList.json', files.CardList),
         writeFile('QuestList.json', files.QuestList),
       ]);
+      // overwrite old if md5 not match
+      if (
+        md5hex(fs.readFileSync(path.join(DATA_DIR, 'FileList.json'))) !==
+        md5hex(fs.readFileSync(path.join(DATA_DIR, 'FileListTmp.json')))
+      ) {
+        fs.copyFileSync(
+          path.join(DATA_DIR, 'FileListTmp.json'),
+          path.join(DATA_DIR, 'FileListOld.json'),
+        );
+      }
       dbs.fileList.read();
       dbs.fileListA.read();
       dbs.cardList.read();
       dbs.questListOrigin.read();
       MutationResolver.downloadFiles();
     }
+  } else {
+    ctx.status = 400;
+    ctx.body = 'File(s) missing!';
+    return;
   }
   ctx.body = 'OK';
 };
