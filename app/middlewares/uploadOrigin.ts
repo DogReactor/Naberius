c1e54c51481690fcfc8db2ada7c52876ecc32a72
MoonDropX <minalinskyx@hotmail.com>
2018-11-28 01:20:26 +0800

    feat(uploadOrigin): accept formdata k-v form submit

--- app/middlewares/uploadOrigin.ts
+++ app/middlewares/uploadOrigin.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs-extra';
 import * as dbs from '../graphql/dataFiles';
 import * as crypto from 'crypto';
 import { DATA_DIR } from '../consts';
+import { logger } from '../logger';
 import MutationResolver from '../graphql/resolvers/MutationResolver';
 
 function md5hex(str: string | Buffer) {
@@ -14,6 +15,9 @@ function md5hex(str: string | Buffer) {
 }
 
 async function writeFile(filename: string, file: any) {
+  if (typeof file === 'string') {
+    return fs.writeFile(path.join(DATA_DIR, filename), file);
+  }
   return new Promise((resolve, reject) => {
     const reader = fs.createReadStream(file.path);
     const stream = fs.createWriteStream(path.join(DATA_DIR, filename));
@@ -28,7 +32,7 @@ export default async (ctx: Context, next: any) => {
   if (ctx.method !== 'POST') {
     return next();
   }
-  // const files = ctx.request.files;
+
   if (ctx.request.files) {
     const files = ctx.request.files;
     if (
@@ -37,41 +41,66 @@ export default async (ctx: Context, next: any) => {
       files.CardList &&
       files.QuestList
     ) {
-      await fs.copy(
-        path.join(DATA_DIR, 'FileList.json'),
-        path.join(DATA_DIR, 'FileListTmp.json'),
-      );
-      // }
+      // file upload
       await Promise.all([
-        writeFile('FileList.json', files.FileListR),
+        writeFile('FileListTmp.json', files.FileListR),
         writeFile('FileListA.json', files.FileListA),
         writeFile('CardList.json', files.CardList),
         writeFile('QuestList.json', files.QuestList),
       ]);
-      dbs.fileList.read();
-      dbs.fileListA.read();
-      dbs.cardList.read();
-      dbs.questListOrigin.read();
-      // overwrite old if md5 not match
-      if (
-        md5hex(fs.readFileSync(path.join(DATA_DIR, 'FileList.json'))) !==
-        md5hex(fs.readFileSync(path.join(DATA_DIR, 'FileListTmp.json')))
-      ) {
-        fs.copyFileSync(
-          path.join(DATA_DIR, 'FileListTmp.json'),
-          path.join(DATA_DIR, 'FileListOld.json'),
-        );
-      }
-      MutationResolver.downloadFiles();
     } else {
-      ctx.status = 400;
-      ctx.body = 'File(s) missing!';
-      return;
+      if (ctx.request.body) {
+        const { FileListR, FileListA, CardList, QuestList } = ctx.request
+          .body as any;
+        if (FileListR && FileListA && CardList && QuestList) {
+          await Promise.all([
+            writeFile('FileListTmp.json', FileListR),
+            writeFile('FileListA.json', FileListA),
+            writeFile('CardList.json', CardList),
+            writeFile('QuestList.json', QuestList),
+          ]);
+        } else {
+          ctx.status = 400;
+          ctx.body = 'File(s) missing!';
+          return;
+        }
+      } else {
+        ctx.status = 400;
+        ctx.body = 'No body!';
+        return;
+      }
     }
   } else {
     ctx.status = 400;
     ctx.body = 'No files uploaded!';
     return;
+  }
+  logger.info('File uploaded');
+  // overwrite old if md5 not match
+  if (
+    md5hex(fs.readFileSync(path.join(DATA_DIR, 'FileList.json'))) !==
+    md5hex(fs.readFileSync(path.join(DATA_DIR, 'FileListTmp.json')))
+  ) {
+    logger.info('Newer file detected, updating...');
+    // copy now to old
+    fs.copyFileSync(
+      path.join(DATA_DIR, 'FileList.json'),
+      path.join(DATA_DIR, 'FileListOld.json'),
+    );
+
+    // copy new to now
+    fs.copyFileSync(
+      path.join(DATA_DIR, 'FileListTmp.json'),
+      path.join(DATA_DIR, 'FileList.json'),
+    );
+
+    dbs.fileList.read();
+    dbs.fileListA.read();
+    dbs.cardList.read();
+    dbs.questListOrigin.read();
+    MutationResolver.downloadFiles();
+  } else {
+    logger.info('File not changed');
   }
   ctx.body = 'OK';
 };
