72391dcaa22d558c7c37cd1cdd67c40d7ac11086
MoonDropX <minalinskyx@hotmail.com>
2019-04-18 10:25:49 +0800

    fix(CardConnector): use clone deep to divide classes

--- app/graphql/connector/CardConnector.ts
+++ app/graphql/connector/CardConnector.ts
@@ -107,9 +107,9 @@ class CardConnector {
       ClassEvo2a: undefined,
       ClassEvo2b: undefined,
     };
-    classes.ClassInit = _.find(classData.data, {
+    classes.ClassInit = _.cloneDeep(_.find(classData.data, {
       ClassID: card.InitClassID,
-    });
+    }));
     // 如果职业存在
     if (classes.ClassInit) {
       // 如果初始职业可以进阶
@@ -117,17 +117,17 @@ class CardConnector {
         // 根据是否需要珠子判断为cc还是觉醒
         if (classes.ClassInit.Data_ExtraAwakeOrb1) {
           if (card.Rare > 2) {
-            classes.ClassEvo = _.find(classData.data, {
+            classes.ClassEvo = _.cloneDeep(_.find(classData.data, {
               ClassID: classes.ClassInit.JobChange,
-            });
+            }));
           } else {
             // 特殊觉醒职业的银，基础职业最大等级55
             classes.ClassInit.MaxLevelUnit = 55;
           }
         } else {
-          classes.ClassCC = _.find(classData.data, {
+          classes.ClassCC = _.cloneDeep(_.find(classData.data, {
             ClassID: classes.ClassInit.JobChange,
-          });
+          }));
 
           if (card.Rare === 2) {
             classes.ClassCC.MaxLevelUnit = 55;
@@ -144,9 +144,9 @@ class CardConnector {
       }
       // 如果cc职业可以进阶
       if (classes.ClassCC && classes.ClassCC.JobChange && card.Rare > 2) {
-        classes.ClassEvo = _.find(classData.data, {
+        classes.ClassEvo = _.cloneDeep(_.find(classData.data, {
           ClassID: classes.ClassCC.JobChange,
-        });
+        }));
         if (card.Rare === 3) {
           classes.ClassCC.MaxLevelUnit = 80;
         } else if (card.Rare === 4) {
@@ -159,12 +159,12 @@ class CardConnector {
       }
       // 如果觉醒职业可以二觉
       if (classes.ClassEvo && classes.ClassEvo.AwakeType1) {
-        classes.ClassEvo2a = _.find(classData.data, {
+        classes.ClassEvo2a = _.cloneDeep(_.find(classData.data, {
           ClassID: classes.ClassEvo.AwakeType1,
-        });
-        classes.ClassEvo2b = _.find(classData.data, {
+        }));
+        classes.ClassEvo2b = _.cloneDeep(_.find(classData.data, {
           ClassID: classes.ClassEvo.AwakeType2,
-        });
+        }));
       }
     }
     return classes;
