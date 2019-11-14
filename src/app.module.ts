22e508770ef96d3b2bf8c6454f304e9a5ba92b56
MoonDropX <minalinskyx@hotmail.com>
2019-11-14 09:44:03 +0800

    feat: update skeleton

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
+})
+export class AppModule {}
