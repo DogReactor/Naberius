c3ec30ebf92b8a3ad11c63339434b363cc8e8526
MoonDropX <minalinskyx@hotmail.com>
2019-11-27 19:22:55 +0800

    fix(config): add default cache & data dir

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -4,4 +4,6 @@ export const Consts: Partial<Config> = {
   ASSETS_BASE_URL: 'http://assets.millennium-war.net',
   HARLEM_TEXT_R_DIR: 'HarlemTextR',
   HARLEM_TEXT_A_DIR: 'HarlemTextA',
+  CACHE_DIR: 'cache',
+  DATA_DIR: 'data',
 };
