48691ae79cf701d577834739354e99019d251e86
MoonDropX <minalinskyx@hotmail.com>
2019-11-27 19:29:29 +0800

    fix(quest): add schema definition to model

--- src/data/models/quest.model.ts
+++ src/data/models/quest.model.ts
@@ -1,38 +1,77 @@
+import { ObjectType, Field, Int } from 'type-graphql';
+
+@ObjectType()
 export class Quest {
+  @Field(type => Int)
   QuestID: number;
+  @Field(type => Int)
   QuestTitle: number;
+  @Field(type => Int)
   Text: number;
+  @Field(type => Int)
   Type: number;
+  @Field(type => Int)
   Charisma: number;
+  @Field(type => Int)
   ActionPoint: number;
+  @Field(type => Int)
   Rank: number;
+  @Field(type => Int)
   Level: number;
+  @Field(type => Int)
   Capacity: number;
+  @Field(type => Int)
   defHP: number;
+  @Field(type => Int)
   defAP: number;
+  @Field(type => Int)
   AppearCondition: number;
+  @Field(type => Int)
   RankExp: number;
+  @Field(type => Int)
   Gold: number;
+  @Field(type => Int)
   MapNo: number;
+  @Field(type => Int)
   EntryNo: number;
+  @Field(type => Int)
   Treasure1: number;
+  @Field(type => Int)
   Treasure2: number;
+  @Field(type => Int)
   Treasure3: number;
+  @Field(type => Int)
   Treasure4: number;
+  @Field(type => Int)
   Treasure5: number;
+  @Field(type => Int)
   UnitLevel: number;
+  @Field(type => Int)
   QuestTerms: number;
+  @Field(type => Int)
   LocationNo: number;
+  @Field(type => Int)
   StartOpenDay: number;
+  @Field(type => Int)
   EndOpenDay: number;
+  @Field(type => Int)
   OpenWeek: number;
+  @Field(type => Int)
   BonusType: number;
+  @Field(type => Int)
   BonusNum: number;
+  @Field(type => Int)
   UnitList: number;
+  @Field(type => Int)
   _HardLevel: number;
+  @Field(type => Int)
   _HardCondition: number;
+  @Field(type => Int)
   _HardInfomation: number;
+  @Field(type => Int)
   _DispIndex: number;
+  @Field(type => Int)
   _HoldingParam: number;
+  @Field(type => Int)
   DangerArea: number;
 }
--- src/quests/quests.resolver.ts
+++ src/quests/quests.resolver.ts
@@ -1,22 +1,21 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
-import { QuestSchema } from './schemas/quest.schema';
 import { Inject } from '@nestjs/common';
 import { DataFileService } from 'data/dataFile.service';
 import { Quest } from 'data/models/quest.model';
 import { Int } from 'type-graphql';
 
-@Resolver(QuestSchema)
+@Resolver(Quest)
 export class QuestsResolver {
   constructor(
     @Inject('QuestList') private readonly questList: DataFileService<Quest>,
   ) {}
 
-  @Query(returns => [QuestSchema])
+  @Query(returns => [Quest])
   quests() {
     return this.questList.data;
   }
 
-  @Query(returns => QuestSchema, { nullable: true })
+  @Query(returns => Quest, { nullable: true })
   quest(@Args({ name: 'QuestID', type: () => Int }) QuestID: number) {
     return this.questList.data.find(quest => quest.QuestID === QuestID);
   }
