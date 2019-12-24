89cae02b6c0cb9c82a505a0b853df4efeaefcb34
MoonDropX <minalinskyx@hotmail.com>
2019-12-24 15:17:53 +0800

    feat: remove old app folder

--- app/graphql/resolvers/SubscriptionResolver.ts
+++
@@ -1,7 +0,0 @@
-import { pubsub, LOG_ADDED } from '../../pubsub';
-
-export default {
-  logAdded: {
-    subscribe: () => pubsub.asyncIterator([LOG_ADDED]),
-  },
-};
--- app/pubsub.ts
+++
@@ -1,5 +0,0 @@
-import { PubSub } from 'apollo-server';
-
-export const LOG_ADDED = 'LOG_ADDED';
-
-export const pubsub = new PubSub();
