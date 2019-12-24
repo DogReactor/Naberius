d45e00de755814f88bf036064122277f8e58b94e
MoonDropX <minalinskyx@hotmail.com>
2019-12-24 16:54:34 +0800

    fix(main): correct static file path

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ParsedConfigService } from 'config/config.service';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -12,7 +13,7 @@ async function bootstrap() {
     prefix: '/static/',
   });
 
-  app.useStaticAssets('static', { prefix: '/static/' });
+  app.useStaticAssets(join(__dirname, '..', 'static'), { prefix: '/static/' });
 
   await app.listen(app.get(ParsedConfigService).get('PORT'));
 }
