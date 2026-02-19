92d7b9405795fcc799202de89c99e03b8b2222c8
Terry Hu <terryhu9992@outlook.com>
2026-02-19 08:34:48 -0600

    File Struture Changed: Unit Icon (on 2026/02/19).

--- src/data/ico.service.ts
+++ src/data/ico.service.ts
@@ -5,39 +5,41 @@ import { ALTXExtractPNG } from 'common/utils';
 import { join } from 'path';
 import { ensureDirSync } from 'fs-extra';
 import { ParsedConfigService } from 'config/config.service';
+import { Repository } from 'typeorm';
+import { File } from './models/file.model';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class IcoService {
   constructor(
+    @InjectRepository(File)
+    private readonly files: Repository<File>,
     private readonly request: RequestService,
     private readonly config: ParsedConfigService,
   ) {}
   async update() {
     try {
-      await Promise.all([
-        this.request.requestFile('ico_00.aar'),
-        this.request.requestFile('ico_01.aar'),
-        this.request.requestFile('ico_02.aar'),
-        this.request.requestFile('ico_03.aar'),
-      ]).then(res => {
-        res.forEach((aar, index) => {
+      await Promise.all(
+        (await this.files.find({where: { Name: /ico_0[0-3]_\d\d\.aar/ }}))
+        .map(async item => {
+          const aar = await this.request.requestFile(item.Name);
           const parsed = parseAL(aar) as ALAR;
-          const dir = join(this.config.get('ICO_DIR'), index.toString());
+          const dir = join(this.config.get('ICO_DIR'), item.Name.substring(5,6));
           ensureDirSync(dir);
           parsed.Files.forEach(file => {
             const atx = file.Content as ALTX;
             Object.keys(atx.Sprites).forEach(key => {
               const sprite = atx.Sprites[Number.parseInt(key, 10)][0];
-              if (sprite.Width !== 0 && sprite.Height !== 0) {
+              if (sprite.Width > 0 && sprite.Height > 0) {
                 const modKey = Number.parseInt(key, 10) & 0x1fff;
                 ALTXExtractPNG(atx,
-                    sprite.X, sprite.Y, sprite.Width, sprite.Height)
-                  .toFile(join(dir, `${modKey}.png`));
+                  sprite.X, sprite.Y, sprite.Width, sprite.Height)
+                .toFile(join(dir, `${modKey}.png`));
               }
             });
           });
-        });
-      });
+        })
+      );
     } catch (e) {
       console.error(e);
     }
